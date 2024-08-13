f24dbac77b7d2cf8486942aa371a6847045fd68a
doxcom <aldoperez1319@gmail.com>
2024-08-13 11:21:04 -0600

    data types folder and files

---
+++ DataTypesJS/script.js
@@ -0,0 +1,67 @@
+//DataTypes
+let js = 'amazing';
+//if (js === 'amazing') alert('Values and 
+//Variables is FUN!');
+
+
+let countryName = "Mexico";
+let continentName = "America";
+let population_Num = 127000000;
+
+console.log(countryName);
+console.log(continentName);
+console.log(population_Num);
+
+//numbers are floating always
+//23, will be 23.0
+let age = 23;
+
+//string, are allowed to declare with single or double quotes ""
+
+let firstName = 'aldo';
+let secondName = "perez";
+
+//boolean type
+//true or false
+let flag_one = true;
+
+true;
+console.log(true);
+console.log("JAVASCRIPT");
+let javascriptIsFun = true;
+
+
+//undefined, without assigning a value
+let children;
+
+//Null
+
+//Symbol
+
+//BigInt
+
+//dynamic typing: not defining a data type
+
+
+//typeOf operator to show the type of the value
+//should print "boolean"
+console.log(typeof true);
+
+//should print undefined
+console.log(typeof children);
+
+//you can edit the current value of a variable, without "let" instruction
+
+javascriptIsFun = 'YES';
+
+//undefined example
+
+let year;
+console.log(year);
+console.log(typeof year);
+year = 1990;
+console.log(typeof year);
+
+//should return null but is return object
+//is an internal bug in js
+console.log(typeof null);
