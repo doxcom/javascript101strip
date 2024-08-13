40a358bebf47a294f3ef014ff67b573debd49de2
doxcom <aldoperez1319@gmail.com>
2024-08-13 10:43:14 -0600

    variables let example

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
-
+//variables examples
 let js = 'amazing';
-if (js === 'amazing') alert('JavaScript is FUN!');
+//if (js === 'amazing') alert('JavaScript is FUN!');
 console.log(100 + 300);
+
+console.log("Jonas");
+console.log(23);
+
+//declaring variables with let
+let firstName = "Bob";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+
+let first = "aldo";
+let num_a = 230;
+//illegal to declare jonas&matia, & is now allowed
+//let new, new is a reserved keyword
+//let function, function is a reserved keyword
+//let name, name is a reserver keyword too
+//let Person, is not allowed not to start with upperCase "P"
+
+//constants are allowed to use known names:
+let PI = 3.1415;
+
+//good example of variable names
+let myFirstJob = 'Technician';
+let myCurrentJob = 'Programmer';
+
+
+
+
+
+
+
 
