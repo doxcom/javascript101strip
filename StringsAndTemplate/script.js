2ec342469d485e96839bb7bd7fee1b99512fa4d1
doxcom <aldoperez1319@gmail.com>
2024-08-16 14:17:12 -0600

    if else example

--- StringsAndTemplate/script.js
+++ StringsAndTemplate/script.js
@@ -14,3 +14,32 @@ console.log(aldo);
 //we can call a variable with ${} template
 const aldoNew = `I'am ${firstName} a ${currentYear - birthYear} year old ${job}!`;
 console.log(aldoNew);
+
+console.log(`Just a regular String..`);
+
+//multiline string
+//\n new line should be created
+console.log('String with \n\
+    multiple \n\
+    lines')
+
+
+console.log(`String
+multiple
+lines`);
+
+//IF ELse Statements
+
+const age = 15;
+// if var contains comparator value it will be assigned as a boolean variable
+
+const isOldEnough = age >= 18; //should b true
+
+if (isOldEnough) {
+    console.log('Sarah can start driving license 🚗');
+} else {
+
+    const yearsLeft = 18 - age;
+    console.log(`Sarah is too young. Wait another ${yearsLeft} years 😁`);
+}
+
