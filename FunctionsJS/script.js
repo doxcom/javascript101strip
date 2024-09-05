d9984e16796738ab4364ac795e0d9a9f212ea660
doxcom <aldoperez1319@gmail.com>
2024-09-05 15:44:22 -0600

    functions with expressions

--- FunctionsJS/script.js
+++ FunctionsJS/script.js
@@ -14,8 +14,8 @@ function fruitProcessor(apples, oranges) {
     return juice;
 }
 
-const appleJuiice = fruitProcessor(5, 0); //we passed arguments the return of juice value should be added into another variable
-console.log(appleJuiice);
+const appleJuice = fruitProcessor(5, 0); //we passed arguments the return of juice value should be added into another variable
+console.log(appleJuice);
 
 console.log(fruitProcessor(5, 0)); //this is another way, without storing in a value
 
@@ -23,4 +23,17 @@ console.log(fruitProcessor(5, 0)); //this is another way, without storing in a v
 const appleOrangeJuice = fruitProcessor(2, 4);
 console.log(appleOrangeJuice);
 
+
+
+function calcAge1(birthYear) {
+    return 2024 - birthYear;
+}
+
+console.log(calcAge1(1990));
+
+//anonymous function, but need to store in a var
+const calcAge2 = function (birthYearB) {
+    return 2030 - birthYearB
+}
+//note: functions can be called before to be declare in code
 
