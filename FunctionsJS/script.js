e31a74729e7e1941a94ff04ca20bf29b4746f5bc
doxcom <aldoperez1319@gmail.com>
2024-09-06 02:15:37 -0600

    arrow function example with parameters

--- FunctionsJS/script.js
+++ FunctionsJS/script.js
@@ -23,17 +23,40 @@ console.log(fruitProcessor(5, 0)); //this is another way, without storing in a v
 const appleOrangeJuice = fruitProcessor(2, 4);
 console.log(appleOrangeJuice);
 
-
+console.log("Edad Rocio Perez", calcAge1(1967))
 
 function calcAge1(birthYear) {
     return 2024 - birthYear;
 }
 
-console.log(calcAge1(1990));
+console.log("Edad aldo perez", calcAge1(1990));
+
+//console.log(calcAge2)
 
 //anonymous function, but need to store in a var
 const calcAge2 = function (birthYearB) {
-    return 2030 - birthYearB
+    return 2024 - birthYearB;
 }
-//note: functions can be called before to be declare in code
+//note: functions can be called before to be declare in code+
+
+
+//ARROW FUNCTION
+//const birthYear = 0;
+const calcAge3 = birthYear => 2037 - birthYear;
+const age3 = calcAge3(1991);
+console.log("funcion arrow", age3);
+
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+
+    const age = 2037 - birthYear;
+    const retirement = 65 - age;
+
+    return `${firstName} retires in ${retirement} years`;
+}
+console.log(yearsUntilRetirement(1990, 'Jonas Perez'));
+console.log(yearsUntilRetirement(2001, 'Johana'));
+
+
+
 
