e6bef4aef118f85e787200732fbc834786caabeb
doxcom <aldoperez1319@gmail.com>
2025-01-10 01:11:44 -0600

    setting up editor

--- DeveloperSkills/script.js
+++ DeveloperSkills/script.js
@@ -7,3 +7,12 @@ const x = '23';
 if (x === 23) console.log(23);
 
 const calAge = birthYear => 2037 - birthYear;
+
+console.log("hello");
+console.log(x);
+
+//FIXME
+//highlight todo -> VIDEO
+
+console.log("hola");
+console.log(1992);
