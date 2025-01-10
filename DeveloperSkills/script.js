6f95f31e41b9abaed79ae4180d3b40cfde7e1c0f
doxcom <aldoperez1319@gmail.com>
2025-01-10 13:04:07 -0600

    adding setup for node js live server

--- DeveloperSkills/script.js
+++ DeveloperSkills/script.js
@@ -8,11 +8,33 @@ if (x === 23) console.log(23);
 
 const calAge = birthYear => 2037 - birthYear;
 
-console.log("hello");
-console.log(x);
+//console.log("hello");
+//console.log(x);
 
 //FIXME
 //highlight todo -> VIDEO
 
-console.log("hola");
-console.log(1992);
+//console.log("hola");
+//console.log(1992);
+
+//Temperature program
+
+const temperatures = [3,2,-6,-1, 'error',9,13,17,15,14,9,5];
+
+const calcTempAmplitude = function(temps){
+    let max= temps[0];
+    let min= temps[0];
+
+    for (let i =0; i<temps.length; i++){
+        const curTemp= temps[i];
+        if(typeof curTemp !== 'number') continue;
+
+        if(curTemp > max) max= curTemp;
+        if(curTemp <min) min = curTemp;
+    }
+
+    console.log("max is ",max,"min is", min);
+
+};
+calcTempAmplitude([3,7,4,1,8]);
+calcTempAmplitude(temperatures);
