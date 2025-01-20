2681148397bcc91b82272e3e7b830091b1453702
doxcom <aldoperez1319@gmail.com>
2025-01-20 15:36:09 -0600

    js code added into game

--- JSDOM/script.js
+++ JSDOM/script.js
@@ -2,5 +2,28 @@
 //control s use prettier to format the code
 'use strict';
 
-document.querySelector('.message');
+/*
+document.querySelector('.message').textContent;
+
+
+document.querySelector('.message').textContent = ' 🎉 Correct Number!';
+
+document.querySelector('.message').textContent;
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 23;3
+*/
+
+document.querySelector('.check').addEventListener('click',function(){
+    const guess = Number(document.querySelector('.guess').value);
+
+    console.log(guess, typeof guess);
+
+    if(!guess){
+        document.querySelector('.message').textContent='No Number Found!';
+    }
+})
+
 
