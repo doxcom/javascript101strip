9c37f4586852a84bb77fa1e0327dcadf95d6a557
doxcom <aldoperez1319@gmail.com>
2025-01-21 00:27:50 -0600

    adding if logic into js

--- JSDOM/script.js
+++ JSDOM/script.js
@@ -16,6 +16,15 @@ document.querySelector('.score').textContent = 10;
 document.querySelector('.guess').value = 23;3
 */
 
+//random number generated to play
+const secretNumber = Math.trunc(Math.random()*20)+1;
+
+let score = 20; //initial score, with let,bc constant is inmutable
+
+document.querySelector('.number').textContent = secretNumber;
+
+
+
 document.querySelector('.check').addEventListener('click',function(){
     const guess = Number(document.querySelector('.guess').value);
 
@@ -23,7 +32,26 @@ document.querySelector('.check').addEventListener('click',function(){
 
     if(!guess){
         document.querySelector('.message').textContent='No Number Found!';
+    }else if(guess === secretNumber){
+        document.querySelector('.message').textContent = ' 🎉 Correct Number!';
+    } else if(guess > secretNumber){
+    if(score > 1){
+        document.querySelector('.message').textContent=
+        '📈 Too High!';
+        score--;
+        document.querySelector('.score').textContent = score;
+    }else{
+        document.querySelector('.message').textContent = 'you You lost the game!';
     }
-})
+
+       
+
+    }else if(guess < secretNumber){
+        document.querySelector('.message').textContent=
+        ' 📉 Too Low!';
+        score--;
+        document.querySelector('.score').textContent = score;
+    }
+});
 
 
