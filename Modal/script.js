1d6fe236415642e82da6540f5e9e1a895f50edf2
doxcom <aldoperez1319@gmail.com>
2025-01-23 02:28:33 -0600

    css style with events from clicks

--- Modal/script.js
+++ Modal/script.js
@@ -2,3 +2,32 @@
 //control s use prettier to format the code
 'use strict';
 
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+console.log(btnsOpenModal);
+
+const openModal= function(){
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+const closeModal = function(){
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
+
+for(let i = 0; i < btnsOpenModal.length; i++)
+    btnsOpenModal[i].addEventListener('click', openModal);
+    //or
+    btnCloseModal.addEventListener('click', closeModal);
+ //to close emergent window message doing clic outside message and 'x' option
+    overlay.addEventListener('click', closeModal);
+
+
+//for keyboard events
+
+
