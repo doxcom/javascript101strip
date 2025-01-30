cf8c650be1b66ed645db2a4f1c9aa983326e06e0
doxcom <aldoperez1319@gmail.com>
2025-01-29 23:36:14 -0600

    scope variables example

---
+++ BehindScencesJS/script2/script.js
@@ -0,0 +1,13 @@
+
+
+console.log(me); //var is allowed to access
+console.log(job);// is not allowed to access
+console.log(year);//is not allowed to access
+
+var me = 'aldo';
+let job = 'programmer';
+const year = 1990;
+
+//FUNCTIONS
+
+
