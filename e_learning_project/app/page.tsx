38597ba43c24bc8577912f9b2e6edd0bc7ad94fe
doxcom <aldoperez1319@gmail.com>
2025-02-14 17:54:11 -0600

    initial project part

---
+++ e_learning_project/app/page.tsx
@@ -0,0 +1,9 @@
+//tutorial https://www.youtube.com/watch?v=34w8xP_cvqg
+
+import React from "react";
+
+const page = () => {
+  return <div className="text-6x1 text-red-700">HomePage</div>;
+};
+
+export default page;
