f367b70ccbc7510c9e0539ac4464fe89b67e6409
doxcom <aldoperez1319@gmail.com>
2025-02-17 01:01:47 -0600

    adding scrollY changing navbar color

--- e_learning_project/components/Home/Navbar/Nav.tsx
+++ e_learning_project/components/Home/Navbar/Nav.tsx
@@ -1,11 +1,34 @@
-import React from "react";
+'use client'
+
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import { navLinks } from "@/constant/constant";
 import Link from "next/link";
+import { HiBars3BottomRight } from "react-icons/hi2";
 
 const Nav = () => {
+  const [navBg, setNavBg] = useState(false);
+
+  useEffect(() => {
+    const handler = () => {
+      if (window.scrollY >= 90) {
+        setNavBg(true);
+      }
+      if (window.scrollY < 90) {
+        setNavBg(false);
+      }
+    };
+    window.addEventListener("scroll",handler);
+
+    return () => window.removeEventListener("scroll", handler);
+  });
+
   return (
-    <div className="fixed w-full transition-all duration-200 h-[12vh] z-[1000] bg-blue-700">
+    <div
+      className={`fixed ${
+        navBg ? 'bg-indigo-800' : "fixed"
+      } w-full transition-all duration-200 h-[12vh] z-[1000] bg-blue-700`}
+    >
       <div className="flex items-center h-full justify-between w-[90%] xl:w-[80%] mx-auto">
         {/*Logo*/}
         <Image src="/images/logo.png" alt="Logo" width={120} height={120} />
@@ -27,6 +50,8 @@ const Nav = () => {
           >
             Signup Now
           </button>
+          {/* Burger menu*/}
+          <HiBars3BottomRight className="w-8 h-8 cursor-pointer text-white lg:hidden" />
         </div>
       </div>
     </div>
