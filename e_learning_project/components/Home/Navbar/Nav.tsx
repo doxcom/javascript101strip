52294118d4ad8e8764d5806bf1533a2000b3f8bd
doxcom <aldoperez1319@gmail.com>
2025-02-16 23:59:58 -0600

    nav bar added

--- e_learning_project/components/Home/Navbar/Nav.tsx
+++ e_learning_project/components/Home/Navbar/Nav.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Image from "next/image";
+import { navLinks } from "@/constant/constant";
+import Link from "next/link";
 
 const Nav = () => {
   return (
@@ -7,6 +9,16 @@ const Nav = () => {
       <div className="flex items-center h-full justify-between w-[90%] xl:w-[80%] mx-auto">
         {/*Logo*/}
         <Image src="/images/logo.png" alt="Logo" width={120} height={120} />
+        {/* NavLinks */}
+        <div className="hidden lg:flex items-center space-x-10">
+          {navLinks.map((link) => {
+            return (
+              <Link key={link.id} href={link.url}>
+                <p className="nav__link">{link.label}</p>
+              </Link>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
