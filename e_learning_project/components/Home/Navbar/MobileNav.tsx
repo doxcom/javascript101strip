f165fc1f87c94a7df99d7bb86dcee6f9957a8031
doxcom <aldoperez1319@gmail.com>
2025-02-18 23:49:36 -0600

    adding navlinks into movile view

--- e_learning_project/components/Home/Navbar/MobileNav.tsx
+++ e_learning_project/components/Home/Navbar/MobileNav.tsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const MobileNav = () => {
-  return <div>MobileNav</div>;
+  return (
+  
+  <div>
+ {/* Overlay*/}
+ <div className="fixed top-0 transform transition-all duration-500 z-[10000] left-0 right-0 bottom-0 bg-black
+ opacity-70 w-full h-[100vh]" />
+ <div className="text-white fixed justify-center flex-col h-full transform transition-al duration-500
+ delay-300 w-[80%] sm:w-[60%] bg-indigo-900 space-y-6 z-[100006]">
+  {/* Navlinks*/}
+  {}
+ </div>
+  </div>
+  );
 };
 
 export default MobileNav;
