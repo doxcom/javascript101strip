b9bad2bf63a3055249a6e5fd40d8313ad4fef9e5
doxcom <aldoperez1319@gmail.com>
2025-03-04 23:57:38 -0600

    adding about on bottom of main page, with learn more button

---
+++ e_learning_project/components/Home/About/About.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { FaArrowRight, FaAward } from 'react-icons/fa'
+
+const About = () => {
+  return (
+    <div className="pt-16 pb-16 ">
+        {/* define grid */}
+        <div className="w-4/5 mx-auto grid grid-cols-1 lg:grid-cols-2 items-center gap-16">
+        {/* 1st part */}
+        <div>
+            <div className="flex items-center space-x-4">
+                <div className="w-12 h-12 bg-rose-600 rounded-full flex items-center justify-center flex-col">
+                    <FaAward className="h-6 w-6 text-white"/>
+                </div>
+                <h1 className="text-xl text-black font-semibold">
+                    Guaranteed and certified
+                </h1>
+            </div>
+            <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-6xl mt-8 font-bold md:leading-[3rem] lg:leading-
+            [3.5rem] xl:leading-[3.9rem] text-gray-800">
+                Online learning wherever and whenever.
+            </h1>
+            <p className="mt-4 text-gray-600">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt laborum eos fugit cupiditate corrupti quos repellendus, unde, labore sit magni laudantium facere distinctio molestias voluptates sapiente officia consectetur esse quo.
+            </p>
+            <button className="flex-items-center space-x-2 px-8 py-3 mt-8 hover:bg-gray-700 transition-all
+            duration-200 rounded-3xl bg-black text-white">
+                <span>Learn More</span>
+                <FaArrowRight/>
+            </button>
+        </div>
+        {/* 2nd part */}
+        <div></div>
+        </div>
+    </div>
+  )
+}
+
+export default About
