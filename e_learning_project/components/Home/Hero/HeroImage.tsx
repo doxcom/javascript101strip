0ca2a447dfce631a4eb7746ffb37cefaf5324fc3
doxcom <aldoperez1319@gmail.com>
2025-03-04 23:20:02 -0600

    adding image with hover on the main page

--- e_learning_project/components/Home/Hero/HeroImage.tsx
+++ e_learning_project/components/Home/Hero/HeroImage.tsx
@@ -1,8 +1,16 @@
+"use client" //this is to make this a client component
 import React from 'react'
+import Tilt from "react-parallax-tilt";
+import Image from 'next/image';
 
+//hidden lg:black will hide our img in small devices
 const HeroImage = () => {
   return (
-    <div>HeroImage</div>
+     <Tilt>      
+      <div className="hidden lg:block">
+        <Image src="/images/hero.png" width={800} height={600} alt="Hero" />
+      </div>
+     </Tilt>
   )
 }
 
