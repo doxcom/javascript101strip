0a6a25eba8b162ace1b9f39af5923dbb0a89e8c6
doxcom <aldoperez1319@gmail.com>
2025-04-24 18:41:32 -0600

    implementing transfer

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -88,9 +88,9 @@ const displayMovements = function (movements){
   });
 };
 
-const calcDisplayBalance = function (movements) {
-  const balance = movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent=`${balance} €`;
+const calcDisplayBalance = function (acc) {
+  acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent=`${acc.balance} €`;
 };
 
 const calcDisplaySummary = function(acc){
@@ -157,12 +157,24 @@ btnLogin.addEventListener('click', function(e){
     displayMovements(currentAccount.movements);
 
     //display balance
-    calcDisplayBalance(currentAccount.movements);
+    calcDisplayBalance(currentAccount);
 
     //display summaryy
     calcDisplaySummary(currentAccount);
   }
 
+});
+
+btnTransfer.addEventListener('click', function(e){
+  e.preventDefault();
+  const amount = Number(inputTransferAmount.value);
+  const receiverAcc = accounts.find(
+    acc => acc.username === inputTransferTo.value
+  );
+   console.log(amount, receiverAcc);
+  if(amount > 0 && currentAccount.balance >= amount && receiverAcc?.username !== currentAccount.username){
+   console.log('Transfer Valid');
+  }
 });
 
 
