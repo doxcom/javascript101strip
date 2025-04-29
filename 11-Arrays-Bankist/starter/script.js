12d97a59421c4d04cf95cf9bfbb7141c69bf9af5
doxcom <aldoperez1319@gmail.com>
2025-04-28 22:42:31 -0600

    fixing value transfer

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -175,7 +175,7 @@ btnTransfer.addEventListener('click', function(e){
   const receiverAcc = accounts.find(
     acc => acc.username === inputTransferTo.value
   );
-   inputTransferAmount.value = inputTransferTo = '';
+   inputTransferAmount.value = inputTransferTo.value = '';
   if(amount > 0 && receiverAcc && currentAccount.balance >= amount && receiverAcc?.username !== currentAccount.username){
    console.log('Transfer Valid');
    //doing the transfer 
