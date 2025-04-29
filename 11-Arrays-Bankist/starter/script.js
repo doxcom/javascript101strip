ded907b9136352ff2db538bd8e10b60f018e2110
doxcom <aldoperez1319@gmail.com>
2025-04-29 01:08:42 -0600

    implementing delete account function

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -186,6 +186,28 @@ btnTransfer.addEventListener('click', function(e){
   }
 });
 
+btnClose.addEventListener('click', function(e){
+  e.preventDefault();
+  console.log('Deleted Account');
+
+
+  if(inputCloseUsername.value === currentAccount.username && Number(inputClosePin.value) === currentAccount.pin){
+    
+    //get index from the array 0,1,2,3...
+    const index = accounts.findIndex(acc => acc.username === currentAccount.username);
+    console.log(index);
+    
+    //Delete account
+    accounts.splice(index, 1);
+  
+    //Hide UI
+    containerApp.style.opacity = 0;
+  }
+
+  inputCloseUsername.value = inputClosePin.value = '';
+
+
+})
 
 /*
 const user = 'Steven Thomas Williams'; //we want to get STW 
