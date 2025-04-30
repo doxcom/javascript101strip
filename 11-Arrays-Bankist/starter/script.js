fda4434be74be5828820e0b18c5e0586245c151c
doxcom <aldoperez1319@gmail.com>
2025-04-29 21:03:12 -0600

    adding challenge with breeds

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -465,4 +465,70 @@ const overalBalance = accounts
   .reduce((acc, mov) => acc + mov,0);
   console.log(overalBalance2);
 
+  //TEST DATA:
 
+  const breeds =[
+  {
+    breed: 'German Shepherd',
+    averageWeight: 32,
+    activities: ['fetch', 'swimming'],
+  },
+  
+  {
+    breed: 'Dalmatian',
+    averageWeight: 24,
+    activities: ['running','fetch', 'swimming'],
+  },
+
+  {
+    breed: 'Labrador',
+    averageWeight: 28,
+    activities: ['swimming', 'fetch'],
+  },
+
+  {
+    breed: 'Beagle',
+    averageWeight: 12,
+    activities: ['digging', 'fetch'],
+  },
+
+  {
+    breed: 'Husky',
+    averageWeight: 36,
+    activities: ['running','agility', 'swimming'],
+  },
+
+  {
+    breed: 'Bulldog',
+    averageWeight: 36,
+    activities: ['sleeping'],
+  },
+
+  {
+    breed: 'Poodle',
+    averageWeight: 18,
+    activities: ['agility', 'fetch'],
+  },
+];
+
+
+const huskyWeight = breeds.find(breed => breed.breed === 'Husky').averageWeight;
+console.log("husky weight :" + huskyWeight);
+
+//2.
+const dogBothActivities = breeds.find(
+  breed => 
+    breed.activities.includes('fetch') && breed.activities.includes('running')).breed;
+  console.log(dogBothActivities);
+
+  //3.
+
+  const allActivities = breeds.flatMap(breed => breed.activities);
+  console.log(allActivities);
+
+  //4 unique activities
+
+  const uniqueActivities = [...new Set(allActivities)];
+  console.log(uniqueActivities);
+
+  
