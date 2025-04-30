02e9410647408540ec7eee7e664799416065e77a
doxcom <aldoperez1319@gmail.com>
2025-04-29 20:25:28 -0600

    flat and flatmap excercise

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -429,3 +429,40 @@ console.log(anyDeposits);
 //every 
 
 console.log(movements.every(mov => mov >0));
+
+
+//nested array
+
+const arr =[[1,2,3], [4,5,6], 7,8];
+
+console.log("nested array in one big array: " + arr.flat()); //  1,2,3,4,5,6,7,8
+
+const arrDeep =[[[1,2],3], [4,[5,6]], 7,8];
+console.log(arrDeep.flat(2)); //works with deep level with a number as a parameter (2)
+/*
+const accountMovements = accounts.map(acc => acc.movements)
+console.log(accountMovements);
+
+const allMovements = accountMovements.flat();
+console.log(allMovements);
+
+const overalBalance = allMovements.reduce((acc,mov)=> acc + mov, 0);
+console.log(overalBalance);
+*/
+
+//flat
+const overalBalance = accounts
+  .map(acc=> acc.movements)
+  .flat()
+  .reduce((acc, mov) => acc + mov,0);
+
+  console.log(overalBalance);
+
+  //flatmap for better performance
+
+  const overalBalance2 = accounts
+  .flatMap(acc=> acc.movements)
+  .reduce((acc, mov) => acc + mov,0);
+  console.log(overalBalance2);
+
+
