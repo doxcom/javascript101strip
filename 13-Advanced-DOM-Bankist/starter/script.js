8bac31be566e93bf89c40d8686cf2eae4cfb92ec
doxcom <aldoperez1319@gmail.com>
2025-07-28 23:55:06 -0600

    adding reveal images, need to fix it too

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -269,13 +269,11 @@ const navHeight = nav.getBoundingClientRect().height;
 const stickyNav = function (entries){
   const [entry] = entries;
    
-  if(!entry.isIntersecting) nav.classList.add
-  ('sticky');
+  if(!entry.isIntersecting) nav.classList.add('sticky');
   else nav.classList.remove('sticky');
 };
 
-const headerObserver = new IntersectionObserver
-(stickyNav,{
+const headerObserver = new IntersectionObserver(stickyNav,{
   root: null,
   threshold: 0,
   rootMargin: `-${navHeight}px` ,
@@ -290,14 +288,12 @@ const allSections = document.querySelectorAll('.section');
 
 const revealSection = function(entries, observer){
 
-  const [entry] = entries;
-  console.log(entry);
-
+ entries.forEach(entry =>{
   if(!entry.isIntersecting) return;
 
    entry.target.classList.remove('section--hidden');
    observer.unobserve(entry.target);
-   
+ });
 };
 
 
@@ -311,6 +307,36 @@ allSections.forEach(function(section){
   sectionObserver.observe(section);
   section.classList.add('section--hidden');
 });
+
+
+//lazy loading images
+
+const imgTargets = document.querySelectorAll('img[data-src]');
+
+const loadImg = function (entries, observer){
+  const [entry] = entries;
+
+  if(!entry.isIntersecting) return;
+
+  //replace src with data-src(from index.html file)
+
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener('load', function() {
+    entry.target.classList.remove('lazy-img');
+  });
+
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg,
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: '-200px'
+});
+
+imgTargets.forEach(img => imgObserver.observe(img));
 
 
 
